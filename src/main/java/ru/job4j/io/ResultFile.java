2bc6395bf09f7c91efe6c375a538c17cb9900acf
REDDEVIL <leo.de@mail.ru>
2022-10-29 23:49:39 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,16 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 1; i <= 10; i++) {
+                out.write(("4 * " + i + " = " + 4 * i).getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
