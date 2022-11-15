0a9bbdbb37f18fbeaea87552aa2c3cd5eda34116
REDDEVIL <leo.de@mail.ru>
2022-11-15 16:13:10 +0300

    2. Анализ доступности сервера. [#859 #360695]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,43 +6,21 @@ import java.util.List;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        try (BufferedReader app =
-                     new BufferedReader(
-                             new FileReader(source))) {
+        try (BufferedReader app = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
             boolean switcher = true;
             List<String> rsl = new ArrayList<>();
             String[] time;
             for (String i = app.readLine(); i != null; i = app.readLine()) {
                 if ((i.contains("400") || i.contains("500")) && switcher) {
                     time = i.split(" ", 2);
-                    rsl.add(time[1]);
+                    out.print(time[1] + ';');
                     switcher = false;
                 }
                 if ((!i.contains("400") && !i.contains("500")) && !switcher) {
                     time = i.split(" ", 2);
-                    rsl.add(time[1]);
+                    out.println(time[1] + ';');
                     switcher = true;
-                }
-            }
-            unavailableOutput(target, rsl);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void unavailableOutput(String target, List<String> readLine) {
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ))) {
-            byte r = 0;
-            for (int i = 0; i < readLine.size(); i++) {
-                    out.print(readLine.get(i));
-                    out.print(";");
-                    r++;
-                if (r >= 2) {
-                    out.println();
-                    r = 0;
                 }
             }
         } catch (IOException e) {
