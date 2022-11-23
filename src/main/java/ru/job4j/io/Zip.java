87ab599d79f2405d6be3362ec27a2d65cbd76c01
REDDEVIL <leo.de@mail.ru>
2022-11-23 20:26:25 +0300

    5.2. Архивировать проект [#861 #363558]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -3,7 +3,6 @@ package ru.job4j.io;
 import java.io.*;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -22,10 +21,30 @@ public class Zip {
         }
     }
 
+    public void validateDEO(String directory, String exclude, String output) {
+        File file = new File(directory);
+        if (!file.exists()) {
+            throw new IllegalArgumentException("Directory does not exist");
+        } else if (!file.isDirectory()) {
+            throw new IllegalArgumentException("Directory is not correct");
+        }
+        if (!exclude.startsWith(".")) {
+            throw new IllegalArgumentException("File extension is incorrect");
+        }
+        if (!output.contains(".zip") && output.length() < 5) {
+            throw new IllegalArgumentException("Incorrect output filename");
+        }
+    }
+
+
     public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
         ArgsName scissorsData = ArgsName.of(args);
-        List<Path> searcher = Search.search(Path.of(scissorsData.get("d")), t -> !t.toFile().getName().startsWith(scissorsData.get("e"))).stream().collect(Collectors.toList());
-        zip.packFiles(searcher, new File(scissorsData.get("o")));
+        String directory = scissorsData.get("d");
+        String exclude = scissorsData.get("e");
+        String output = scissorsData.get("o");
+        zip.validateDEO(directory, exclude, output);
+        List<Path> searcher = Search.search(Path.of(directory), t -> !t.toFile().getName().startsWith(exclude));
+        zip.packFiles(searcher, new File(output));
     }
 }
