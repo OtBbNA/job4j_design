77d2db3cabfe5bbcba87bed8740e84b11a60c88e
REDDEVIL <leo.de@mail.ru>
2022-12-13 20:53:13 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -14,8 +14,17 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("msg=Bye")) {
-                            server.close();
+                        if (str.contains("msg=")) {
+                            String msg;
+                            String[] buf = str.split("msg=", 2);
+                            String[] buf1 = buf[1].split(" ", 2);
+                            msg = buf1[0];
+                            switch (msg) {
+                                case "Exit" -> server.close();
+                                case "Hello" -> out.write("Hello.".getBytes());
+                                case "What" -> out.write("What.".getBytes());
+                                default -> out.write(msg.getBytes());
+                            }
                         }
                         System.out.println(str);
                     }
