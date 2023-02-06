1690c1edcf17d1f3306b7a06aba49437f63b2afc
REDDEVIL <leo.de@mail.ru>
2023-02-06 22:40:47 +0300

    3. Формат XML [#313165]

--- src/main/java/ru/job4j/serialization/json/JsonMyExample.java
+++ src/main/java/ru/job4j/serialization/json/JsonMyExample.java
@@ -1,6 +1,7 @@
 package ru.job4j.serialization.json;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class JsonMyExample {
     private boolean magazine;
@@ -8,16 +9,18 @@ public class JsonMyExample {
     private String firingMode;
     private Cartridges cartridges;
     private Cartridges[] rsl;
+    private List<String> barrelFeatures;
 
 
     public JsonMyExample(boolean magazine, int numberOfRounds,
-                         String firingMode, Cartridges cartridges) {
+                         String firingMode, Cartridges cartridges, List<String> barrelFeatures) {
         this.magazine = magazine;
         this.numberOfRounds = numberOfRounds;
         this.firingMode = firingMode;
         this.cartridges = cartridges;
         this.rsl = new Cartridges[numberOfRounds];
         Arrays.fill(rsl, cartridges);
+        this.barrelFeatures = barrelFeatures;
     }
 
 
