67abe74fc80192c0621ea993fb8fa560e7a0c38f
REDDEVIL <leo.de@mail.ru>
2023-09-05 01:45:40 +0300

    9. Подзапросы [#504874]

---
+++ db/Subqueries.sql
@@ -0,0 +1,31 @@
+CREATE TABLE customers(
+    id serial primary key,
+    first_name text,
+    last_name text,
+    age int,
+    country text
+);
+
+insert into customers (first_name, last_name, age, country)
+values ('Alexandr', 'Petrishev', 26, 'Russia');
+insert into customers (first_name, last_name, age, country)
+values ('Maxim', 'Zibrov', 29, 'Belarus');
+insert into customers (first_name, last_name, age, country)
+values ('Maria', 'Bolshova', 31, 'Russia');
+
+CREATE TABLE orders(
+    id serial primary key,
+    amount int,
+    customer_id int references customers(id)
+);
+
+insert into orders (amount, customer_id)
+values (5, 1);
+insert into orders (customer_id)
+values (2);
+insert into orders (amount, customer_id)
+values (15, 3);
+
+SELECT (SELECT first_name FROM customers c WHERE (o.id = c.id)), amount
+FROM orders o
+WHERE o.amount IS NULL;
