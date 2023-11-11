400276bf06fcf8cab845377759a1963eb01e3684
REDDEVIL <leo.de@mail.ru>
2023-11-11 12:49:02 +0300

    6. Циклический итератор [#505082]

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,31 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CyclicIterator<T> implements Iterator<T> {
+
+    private int index;
+    private List<T> data;
+
+    public CyclicIterator(List<T> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return data.size() > 0;
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        if (index >= data.size()) {
+            index = 0;
+        }
+        return data.get(index++);
+    }
+}
