6b1e4a2e511214029d18742f1b02ab146d3ed438
OtBbNA <tosha.kostylev.97@mail.ru>
2024-03-22 17:29:41 +0300

    2. Принципы Kiss, Dry и Yagni [#238813 #487490]

--- src/test/java/ru/job4j/kiss/fool/FoolTest.java
+++ src/test/java/ru/job4j/kiss/fool/FoolTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.*;
 class FoolTest {
 
     @Test
-    void AllAnswerBefore10() {
+    void allAnswerBefore10() {
         List<String> expected = List.of("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz");
         for (int i = 0; i < 10; i++) {
             assertThat(Fool.answer(i + 1)).isEqualTo(expected.get(i));
@@ -20,7 +20,7 @@ class FoolTest {
     }
 
     @Test
-    void AllAnswerBefore100() {
+    void allAnswerBefore100() {
         List<String> expected = List.of("91", "92", "Fizz", "94", "Buzz", "Fizz", "97", "98", "Fizz", "Buzz");
         for (int i = 0; i < 10; i++) {
             assertThat(Fool.answer(i + 91)).isEqualTo(expected.get(i));
